44abc5d9697c56e27ec211d23ce689d5014675e9
godbs129 <junhaeyoon@gmail.com>
2020-11-24 17:02:35 +0900

    asdf

--- model/device.js
+++
@@ -1,5 +0,0 @@
-const model = require('./dbcon/dbcon');
-
-exports.deviceinsert = (req,res,next)=>{
-    const {deviceName, deviceToken, userId, typeName}
-}
--- router/user.js
+++ router/user.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const hash = require('../middleware/hash');
-const user = require('../model/user');
+const user = require('../api/user');
 const auth = require('../middleware/auth');
 
 
 router.post('/signin', hash, user.signin, auth.authrize);
 router.post('/signup', hash, user.signup);
-router.post('/autosignin', aut,h.authmiddleware, auth.authrize);
+router.post('/autosignin', auth.authmiddleware, auth.authrize);
 
 module.exports = router;
